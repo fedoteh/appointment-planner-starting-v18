7ad8d329d3a3fbddfcbf4d98290c56d70b03e9f0
fedoteh <fedebenavidez@live.com>
2023-05-19 23:54:55 -0300

    Fix tile styling

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      <p className="tile-title">
+      <p className="tile tile-title">
         {name}
       </p>
       {Object.values(description).map((value,index) => (
